95455029233915d592914551c7fdcad3b744c8b3
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-07-28 16:51:12 -0400

    add node-sass and scss script

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        ROMAN UZAL
+        <p className="title">ROMAN UZAL</p>
       </header>
     </div>
   );
