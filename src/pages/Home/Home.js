26878df2469f7c04b844bc9b75790768ad413086
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-09-06 11:51:14 -0400

    sizing reviews - need mobile heightless style

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -164,10 +164,10 @@ const Home = () => {
       {/* REVIEW */}
       <div className="container m-auto h-auto">
         <div className="overflow-x-auto overflow-y-hidden relative  ">
-          <div className=" w-[1536px] flex row ">
+          <div className="w-[1536px] h-36 2xl:w-[1836px] flex row ">
             {/* REVIEW LIGHT */}
-            <div className="flex w-96 border border-gray-200 shadow-sm ">
-              <figure className="w-full flex justify-center items-center p-4 text-center bg-white border-b border-gray-200">
+            <div className="flex w-auto border border-gray-200 shadow-sm ">
+              <figure className="w-96 flex justify-center items-center p-4 text-center bg-white border-b border-gray-200">
                 <figcaption className="flex w-1/4 justify-center items-center space-x-3">
                   <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 1" />
                 </figcaption>
@@ -182,8 +182,8 @@ const Home = () => {
               </figure>
             </div>
             {/* REVIEW DARK */}
-            <div className="flex w-96 border shadow-sm border-gray-700 ">
-              <figure className="w-full flex justify-center items-center p-4 text-center  bg-gray-800 border-gray-700">
+            <div className="flex w-auto border shadow-sm border-gray-700 ">
+              <figure className="w-96 flex justify-center items-center p-4 text-center  bg-gray-800 border-gray-700">
                 <figcaption className="flex w-1/4 justify-center items-center space-x-3">
                   <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 2" />
                 </figcaption>
@@ -198,8 +198,8 @@ const Home = () => {
               </figure>
             </div>
             {/* REVIEW LIGHT */}
-            <div className="flex w-96 border border-gray-200 shadow-sm  ">
-              <figure className="w-full flex justify-center items-center p-4 text-center bg-white border-b border-gray-200">
+            <div className="flex w-auto border border-gray-200 shadow-sm  ">
+              <figure className="w-96 flex justify-center items-center p-4 text-center bg-white border-b border-gray-200">
                 <figcaption className="flex w-1/4 justify-center items-center space-x-3">
                   <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 3" />
                 </figcaption>
@@ -214,8 +214,40 @@ const Home = () => {
               </figure>
             </div>
             {/* REVIEW DARK */}
-            <div className="flex w-96 border shadow-sm border-gray-700 ">
-              <figure className="w-full flex justify-center items-center p-4 text-center  bg-gray-800 border-gray-700">
+            <div className="flex w-auto border shadow-sm border-gray-700 ">
+              <figure className="w-96 flex justify-center items-center p-4 text-center  bg-gray-800 border-gray-700">
+                <figcaption className="flex w-1/4 justify-center items-center space-x-3">
+                  <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 2" />
+                </figcaption>
+                <div className="flew-col w-3/4">
+                  <blockquote className="text-left italic ml-4 max-w-2xl text-gray-200">
+                    <p className="my-4 font-light">"If you care for your time, I hands down would go with this."</p>
+                  </blockquote>
+                  <div className="space-y-0.5 font-medium text-right">
+                    <div className="text-sm font-light text-gray-200">Bonnie Green</div>
+                  </div>
+                </div>
+              </figure>
+            </div>
+            {/* REVIEW LIGHT */}
+            <div className="flex w-auto border border-gray-200 shadow-sm  ">
+              <figure className="w-96 flex justify-center items-center p-4 text-center bg-white border-b border-gray-200">
+                <figcaption className="flex w-1/4 justify-center items-center space-x-3">
+                  <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 3" />
+                </figcaption>
+                <div className="flew-col w-3/4">
+                  <blockquote className="text-left italic ml-4 max-w-2xl text-gray-500 ">
+                    <p className="my-4 font-light">"If you care for your time, I hands down would go with this."</p>
+                  </blockquote>
+                  <div className="space-y-0.5 font-medium text-right">
+                    <div className="text-sm font-light text-gray-500 ">Bonnie Green</div>
+                  </div>
+                </div>
+              </figure>
+            </div>
+            {/* REVIEW DARK */}
+            <div className="flex w-auto border shadow-sm border-gray-700 ">
+              <figure className="w-96 flex justify-center items-center p-4 text-center  bg-gray-800 border-gray-700">
                 <figcaption className="flex w-1/4 justify-center items-center space-x-3">
                   <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 2" />
                 </figcaption>
