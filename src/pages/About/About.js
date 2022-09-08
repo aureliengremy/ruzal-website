342dba6ff1e689ee97673fb5fa494520476a967e
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-09-08 12:48:39 -0400

    styling description par - page About

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -6,16 +6,16 @@ const About = () => {
   return (
     <div>
       <Navbars />
-      <div className="container m-auto">
+      <div className="container m-auto py-6">
         <div class="flex flex-col md:flex-row bg-white">
-          <div class="p-6 flex flex-col justify-center w-1/2">
+          <div class="p-12 flex flex-col justify-center md:w-1/2 2xl:w-full 2xl:p-44">
             <h5 class="text-gray-900 text-xl font-medium mb-2">I'm title</h5>
             <p class="text-gray-700 text-base mb-4">
             Born in France with a father from Spain, Roman is a natural mover because of his hip-hop and contemporary dance background in which he interacts with the Unknown, as he calls it. This hard-working and mysterious model enjoys taking risks and discovering different aspect of himself when in front of the camera. Roman has shot for Aldo Shoes.
             </p>
             <p class="text-gray-600 text-xs text-right italic">- Roman Uzal</p>
           </div>
-          <img class="w-1/2 h-72 md:h-auto object-cover" src="https://images.squarespace-cdn.com/content/v1/620e74d7c40768323153fba5/56194fee-7329-4bd6-8ef7-2670db26238d/Roman_6372%2Bcopie.jpg" alt="" />
+          <img class="md:w-1/2 h-52 md:h-auto object-cover" src="https://images.squarespace-cdn.com/content/v1/620e74d7c40768323153fba5/56194fee-7329-4bd6-8ef7-2670db26238d/Roman_6372%2Bcopie.jpg" alt="" />
         </div>
       </div>
       <div className="container m-auto">
