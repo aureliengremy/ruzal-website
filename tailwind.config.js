bbfc1bcfb6f3ca1c78371c0512cffde3b248590c
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-09-13 12:07:44 -0400

    add custom colors and bg-color - tailwing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,19 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'split-black-gray': "linear-gradient(to left, #fafafa 50%, #2C272E 50%);"
+        'split-black-gray': "linear-gradient(to left, #fafafa 50%, #2C272E 50%)"
       },
       backgroundColor: {
-        'custom-gray': "#2C272E"
+        'custom-gray': "#2C272E",
+        'custom-white': "#fafafa",
+        'custom-purple': "#753188",
+        'custom-orange': "#e59934"
       },
       colors: {
-        'custom-orange': '#E59934'
+        'custom-orange': '#E59934',
+        'custom-white': "#fafafa",
+        'custom-purple': "#753188",
+        'custom-gray': "#2C272E"
       }
     },
   },
