36458b2de4478748926a48e7f15cdf84edbb7c53
Aurelien Gremy <gremy.aurelien@gmail.com>
2022-10-13 16:16:17 -0400

    create component reviews

---
+++ src/pages/Home/components/Reviews.js
@@ -0,0 +1,112 @@
+import React from 'react'
+
+const Reviews = () => {
+  return (
+    <div className="bg-white py-28">
+    <div className="container m-auto h-auto">
+      <div className="overflow-x-auto overflow-y-hidden relative  ">
+        <div className="w-[1536px] h-36 2xl:w-[1836px] flex row ">
+          {/* REVIEW LIGHT */}
+          <div className="flex w-auto border border-gray-200 shadow-sm ">
+            <figure className="w-96 flex justify-center items-center p-4 text-center bg-white border-b border-gray-200">
+              <figcaption className="flex w-1/4 justify-center items-center space-x-3">
+                <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 1" />
+              </figcaption>
+              <div className="flew-col w-3/4">
+                <blockquote className="text-left italic ml-4 max-w-2xl text-gray-500 dark:text-gray-400">
+                  <p className="my-4 font-light">"If you care for your time, I hands down would go with this."</p>
+                </blockquote>
+                <div className="space-y-0.5 font-medium  text-right">
+                  <div className="text-sm font-light text-gray-500 dark:text-gray-400">Bonnie Green</div>
+                </div>
+              </div>
+            </figure>
+          </div>
+          {/* REVIEW DARK */}
+          <div className="flex w-auto border shadow-sm border-gray-700 ">
+            <figure className="w-96 flex justify-center items-center p-4 text-center  bg-gray-800 border-gray-700">
+              <figcaption className="flex w-1/4 justify-center items-center space-x-3">
+                <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 2" />
+              </figcaption>
+              <div className="flew-col w-3/4">
+                <blockquote className="text-left italic ml-4 max-w-2xl text-gray-200">
+                  <p className="my-4 font-light">"If you care for your time, I hands down would go with this."</p>
+                </blockquote>
+                <div className="space-y-0.5 font-medium text-right">
+                  <div className="text-sm font-light text-gray-200">Bonnie Green</div>
+                </div>
+              </div>
+            </figure>
+          </div>
+          {/* REVIEW LIGHT */}
+          <div className="flex w-auto border border-gray-200 shadow-sm  ">
+            <figure className="w-96 flex justify-center items-center p-4 text-center bg-white border-b border-gray-200">
+              <figcaption className="flex w-1/4 justify-center items-center space-x-3">
+                <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 3" />
+              </figcaption>
+              <div className="flew-col w-3/4">
+                <blockquote className="text-left italic ml-4 max-w-2xl text-gray-500 ">
+                  <p className="my-4 font-light">"If you care for your time, I hands down would go with this."</p>
+                </blockquote>
+                <div className="space-y-0.5 font-medium text-right">
+                  <div className="text-sm font-light text-gray-500 ">Bonnie Green</div>
+                </div>
+              </div>
+            </figure>
+          </div>
+          {/* REVIEW DARK */}
+          <div className="flex w-auto border shadow-sm border-gray-700 ">
+            <figure className="w-96 flex justify-center items-center p-4 text-center  bg-gray-800 border-gray-700">
+              <figcaption className="flex w-1/4 justify-center items-center space-x-3">
+                <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 2" />
+              </figcaption>
+              <div className="flew-col w-3/4">
+                <blockquote className="text-left italic ml-4 max-w-2xl text-gray-200">
+                  <p className="my-4 font-light">"If you care for your time, I hands down would go with this."</p>
+                </blockquote>
+                <div className="space-y-0.5 font-medium text-right">
+                  <div className="text-sm font-light text-gray-200">Bonnie Green</div>
+                </div>
+              </div>
+            </figure>
+          </div>
+          {/* REVIEW LIGHT */}
+          <div className="flex w-auto border border-gray-200 shadow-sm  ">
+            <figure className="w-96 flex justify-center items-center p-4 text-center bg-white border-b border-gray-200">
+              <figcaption className="flex w-1/4 justify-center items-center space-x-3">
+                <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 3" />
+              </figcaption>
+              <div className="flew-col w-3/4">
+                <blockquote className="text-left italic ml-4 max-w-2xl text-gray-500 ">
+                  <p className="my-4 font-light">"If you care for your time, I hands down would go with this."</p>
+                </blockquote>
+                <div className="space-y-0.5 font-medium text-right">
+                  <div className="text-sm font-light text-gray-500 ">Bonnie Green</div>
+                </div>
+              </div>
+            </figure>
+          </div>
+          {/* REVIEW DARK */}
+          <div className="flex w-auto border shadow-sm border-gray-700 ">
+            <figure className="w-96 flex justify-center items-center p-4 text-center  bg-gray-800 border-gray-700">
+              <figcaption className="flex w-1/4 justify-center items-center space-x-3">
+                <img className="w-auto h-auto rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile 2" />
+              </figcaption>
+              <div className="flew-col w-3/4">
+                <blockquote className="text-left italic ml-4 max-w-2xl text-gray-200">
+                  <p className="my-4 font-light">"If you care for your time, I hands down would go with this."</p>
+                </blockquote>
+                <div className="space-y-0.5 font-medium text-right">
+                  <div className="text-sm font-light text-gray-200">Bonnie Green</div>
+                </div>
+              </div>
+            </figure>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Reviews
